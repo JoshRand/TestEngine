de0afde306092c532bb312f5db005b66bf69db99
Joshua anderson <Mp4ct@live.com>
2021-01-06 12:26:07 -0600

    Added mouse position/clicks input callbacks

--- Source/Core/Input.cpp
+++ Source/Core/Input.cpp
@@ -5,12 +5,27 @@
 #include <iostream>
 #include "Window.h"
 Window window;
+
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+void mouse_pos_callback(GLFWwindow* window, double xpos, double ypos);
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
 
 void Input::Init(GLFWwindow* wind)
 {
 	window = wind;
 	glfwSetKeyCallback(window, key_callback);
+	glfwSetCursorPosCallback(window, mouse_pos_callback);
+	glfwSetMouseButtonCallback(window, mouse_button_callback);
+}
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods) 
+{
+	std::cout << "Mouse click button: " << button << std::endl;
+}
+
+void mouse_pos_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	std::cout << "Mouse pos: X= " << xpos << " Y= " << ypos << std::endl;
 }
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
